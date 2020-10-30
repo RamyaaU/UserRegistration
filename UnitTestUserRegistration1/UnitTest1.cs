31a6552018a2eb177e592377b25336e33a270993
Ramya.U <ramyayadav2016@gmail.com>
2020-10-30 13:46:42 +0530

    [Ramya] Add . Added UC_3

---
+++ UnitTestUserRegistration1/UnitTest1.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationWithMSTest;
+
+namespace UnitTestUserRegistration
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFirstName()
+        {
+            //Arrange
+            Validation pattern = new Validation();
+            bool expected = true;
+            string input = "Ramya";
+            //Act
+            bool var = pattern.FirstNameValidation(input);
+            //Assert
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestLastName()
+        {
+            //Arrange
+            Validation pattern = new Validation();
+            bool expected = true;
+            string input = "Singh";
+            //Act
+            bool var = pattern.LastNameValidation(input);
+            //Assert
+            Assert.AreEqual(expected, var);
+        }
+        [TestMethod]
+        public void TestEmailId()
+        {
+            //Arrange
+            Validation pattern = new Validation();
+            bool expected = true;
+            string input = "abc.xyz@yahoo.com";
+            //Act
+            bool var = pattern.EmailidValidation(input);
+            //Assert
+            Assert.AreEqual(expected, var);
+        }
+    }
+}
---
+++ UserRegistration/Validation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationWithMSTest
+{
+    public class Validation
+    {
+        /// <summary>
+        /// Firsts the name validation.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns></returns>
+        public Boolean FirstNameValidation(string firstName)
+        {
+            Regex regularExpression = new Regex("^[A-Z][a-z]{2,}$");
+            if (regularExpression.IsMatch(firstName))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Lasts the name validation.
+        /// </summary>
+        /// <param name="lastName">The last name.</param>
+        /// <returns></returns>
+        public Boolean LastNameValidation(string lastName)
+        {
+
+            Regex regularExpression = new Regex("^[A-Z][a-z]{2,}$");
+            if (regularExpression.IsMatch(lastName))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Emailids the validation.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns></returns>
+        public Boolean EmailidValidation(string email)
+        {
+            Regex regularExpression = new Regex("^[0-9A-Za-z]+([+-_.][a-zA-Z]+)*[@][0-9A-Za-z]+[.][0-9A-Za-z]{2,3}$");
+            if (regularExpression.IsMatch(email))
+                return true;
+            else
+                return false;
+        }
+    }
+}
